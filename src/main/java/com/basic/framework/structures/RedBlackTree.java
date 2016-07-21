f3b0b662e526265261b994cc30eb441c26deb8a1
efilmam <filip.mamic@ericsson.com>
2016-07-21 17:07:52 +0100

    RBT - right rotate

--- src/main/java/com/basic/framework/structures/RedBlackTree.java
+++ src/main/java/com/basic/framework/structures/RedBlackTree.java
@@ -2,8 +2,36 @@ package com.basic.framework.structures;
 
 public class RedBlackTree<T extends Comparable<T>> extends SearchTree<T> {
 
+    private Node<T> root;
+
     public RedBlackTree(final T data) {
         super(data);
+    }
+
+    private void leftRotate(final Node<T> node) {
+
+    }
+
+    private void rightRotate(final Node<T> node) {
+        if (node.getNodeLeft() != null) {
+            final Node<T> rotate = node.getNodeLeft();
+
+            node.setNodeLeft(rotate.getNodeRight());
+            rotate.setNodeRight(node);
+
+            if (node.getParent() == null) {
+                this.root = rotate;
+            }
+            else if (node.getParent().getNodeLeft().equals(node)) {
+                node.getParent().setNodeLeft(rotate);
+            }
+            else {
+                node.getParent().setNodeRight(rotate);
+            }
+
+            rotate.setParent(node.getParent());
+            node.setParent(rotate);
+        }
     }
 
     @Override
@@ -50,6 +78,15 @@ public class RedBlackTree<T extends Comparable<T>> extends SearchTree<T> {
 
         private COLOR color = COLOR.RED;
 
+        public enum COLOR {
+            RED,
+            BLACK
+        }
+
+        public Node(final T data, final SearchTree.Node<T> parent, final SearchTree.Node<T> left, final SearchTree.Node<T> right) {
+            super(data, parent, left, right);
+        }
+
         public COLOR getColor() {
             return color;
         }
@@ -58,13 +95,28 @@ public class RedBlackTree<T extends Comparable<T>> extends SearchTree<T> {
             this.color = color;
         }
 
-        public enum COLOR {
-            RED,
-            BLACK
+        public Node<T> getNodeLeft() {
+            return (Node<T>) this.left;
         }
 
-        public Node(final T data, final SearchTree.Node<T> parent, final SearchTree.Node<T> left, final SearchTree.Node<T> right) {
-            super(data, parent, left, right);
+        public Node<T> getNodeRight() {
+            return (Node<T>) this.right;
+        }
+
+        public Node<T> getParent() {
+            return (Node<T>) this.parent;
+        }
+
+        public void setParent(final Node<T> node) {
+            this.parent = node;
+        }
+
+        public void setNodeLeft(final Node<T> node) {
+            this.left = node;
+        }
+
+        public void setNodeRight(final Node<T> node) {
+            this.right = node;
         }
     }
 }
