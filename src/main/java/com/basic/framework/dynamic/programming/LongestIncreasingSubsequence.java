8df21dc407eaced68fe63cbdb0664bc2df6708f1
fmamic <filipmamic@hotmail.com>
2016-09-12 21:58:39 +0100

    longest sequence

--- src/main/java/com/basic/framework/dynamic/programming/LongestIncreasingSubsequence.java
+++ src/main/java/com/basic/framework/dynamic/programming/LongestIncreasingSubsequence.java
@@ -1,7 +1,28 @@
 package com.basic.framework.dynamic.programming;
 
-public class LongestIncreasingSubsequence {
+class LongestIncreasingSubsequence {
 
+    int longestIncreasingSubsequence(final int[] sequence) {
 
+        final int[] state = new int[sequence.length];
+        int maxNumber = Integer.MIN_VALUE;
+
+        state[0] = 1;
+
+        for (int i = 1; i < sequence.length; i++) {
+            state[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(sequence[j] <= sequence[i] && state[j] + 1 > state[i]) {
+                    state[i] = state[j] + 1;
+                }
+            }
+
+            if(state[i] > maxNumber) {
+                maxNumber = state[i];
+            }
+        }
+
+        return maxNumber;
+    }
 
 }
--- test/com/basic/framework/dynamic/programming/DynamicProgrammingTest.java
+++ test/com/basic/framework/dynamic/programming/DynamicProgrammingTest.java
@@ -28,7 +28,7 @@ public class DynamicProgrammingTest {
     @Test
     public void rodCutter() {
         final RodCutter rodCutter = new RodCutter();
-        final int[] price = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+        final int[] price = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
 
         assertEquals(30, rodCutter.optimalRodCutter(price, 10));
         assertEquals(18, rodCutter.optimalRodCutter(price, 7));
@@ -43,7 +43,7 @@ public class DynamicProgrammingTest {
         System.out.println("Where to cut rod:");
 
         int number = 7;
-        while(number > 0) {
+        while (number > 0) {
             System.out.println(result.getOptimalCut()[number]);
             number = number - result.getOptimalCut()[number];
         }
@@ -52,11 +52,20 @@ public class DynamicProgrammingTest {
     @Test
     public void coinsSum() {
         final CoinsSum coinsSum = new CoinsSum();
-        final int[] coins = new int[] { 1, 3, 5 };
+        final int[] coins = new int[]{1, 3, 5};
 
         assertEquals(3, coinsSum.minimumNumberOfCoins(coins, 11));
         assertEquals(3, coinsSum.minimumNumberOfCoins(coins, 9));
         assertEquals(2, coinsSum.minimumNumberOfCoins(coins, 8));
         assertEquals(2, coinsSum.minimumNumberOfCoins(coins, 10));
+    }
+
+    @Test
+    public void longestIncreasingSubsequenceTest() {
+        final LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence();
+
+        final int[] sequence = new int[]{5, 3, 4, 8, 6, 7};
+
+        assertEquals(4, longestIncreasingSubsequence.longestIncreasingSubsequence(sequence));
     }
 }
